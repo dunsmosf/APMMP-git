bae788e9c7f2432c66fd79115d2d49ac1535c18f
Benton Mize <bentonmize@PMD-Admins-MacBook-Pro.local>
2014-12-02 12:08:41 -0500

    SysMan - Basal constraint
    Reverting back to 0.5 U/Hr limit and 1.0 U/Hr TOO HIGH limit

--- dias/SysMan/src/edu/virginia/dtc/SysMan/Constraints.java
+++ dias/SysMan/src/edu/virginia/dtc/SysMan/Constraints.java
@@ -11,12 +11,12 @@ public class Constraints {
 	// We limit on boluses above this range but under the "too high" value
 	public static final double MAX_MEAL = 18.0;
 	public static final double MAX_CORR = 6.0;
-	public static final double MAX_BASAL = 1.0;
+	public static final double MAX_BASAL = 0.5;
 	public static final double MAX_TOTAL = MAX_MEAL + MAX_CORR + MAX_BASAL;
 	
 	// We exit on boluses above this range
 	public static final double TOO_HIGH_MEAL = 30;
 	public static final double TOO_HIGH_CORR = 30;
-	public static final double TOO_HIGH_BASAL = 2.0;
+	public static final double TOO_HIGH_BASAL = 1.0;
 	public static final double TOO_HIGH_TOTAL = TOO_HIGH_MEAL + TOO_HIGH_CORR + TOO_HIGH_BASAL;
 }
