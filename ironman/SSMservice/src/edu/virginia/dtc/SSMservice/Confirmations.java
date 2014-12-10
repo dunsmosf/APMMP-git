a646941b3cfa7ac716a05da56a21a9907c230717
Benton Mize <bentonmize@PMD-Admins-MacBook-Pro.local>
2014-12-10 12:38:38 -0500

    Deleting ironman
    He will soon be replace by IronMan anyway…

--- ironman/SSMservice/src/edu/virginia/dtc/SSMservice/Confirmations.java
+++
@@ -1,9 +0,0 @@
-package edu.virginia.dtc.SSMservice;
-
-public class Confirmations {
-
-	// Values used in the "status" field of the Confirmation Broadcast intent
-	public static final int CONFIRMATION_TIMED_OUT = -2;
-	public static final int CONFIRMATION_CANCEL = -1;
-	public static final int CONFIRMATION_ACCEPT = 0;	
-}
